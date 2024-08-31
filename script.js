e6f4056ad0c643e9745f8ba050a7301fe795f551
hanzla-s-786 <166967668+hanzla-s-786@users.noreply.github.com>
2024-08-31 22:14:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ function createPassword() {
   }
   passwordbox.value = password;
 }
+
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("password");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+}
+
+
